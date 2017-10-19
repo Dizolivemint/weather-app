365f8eccd1ad4c6a31b3ab8e991546fb9dd8c09b
Miles Exner <Dizolivemint@users.noreply.github.com>
2017-10-18 23:01:51 -0700

    Link Weather and Darksky API callbacks

--- app.js
+++ app.js
@@ -16,23 +16,19 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//   if (errorMessage) {
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
-
-var lng = 37.8267;
-var lat = -122.4233;
-
-weather.getWeather(lng, lat, (errorMessage, results) => {
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(`Currently, the weather is ${results.summary}.`);
-    console.log(`It is ${results.temperature} degrees, but feels like ${results.apparentTemperature} degrees.`);
-    console.log(`The humidity is ${results.humidity} percent with a Dew point at ${results.dewPoint}`);
+    console.log(results.address);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, results) => {
+      if (errorMessage) {
+        console.log(errorMessage);
+      } else {
+        console.log(`Currently, the weather is ${results.summary}.`);
+        console.log(`It is ${results.temperature} degrees, and feels like ${results.apparentTemperature} degrees.`);
+        console.log(`The humidity is ${results.humidity} percent with a Dew point at ${results.dewPoint}`);
+      }
+    });
   }
 });
